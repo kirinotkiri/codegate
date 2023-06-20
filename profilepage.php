58112fac0999802094ece691fa539e4cdab8535c
KaiZuke <38561387+KaiZuke@users.noreply.github.com>
2023-06-20 22:54:55 +0700

    Fixed Navbar

--- profilepage.php
+++ profilepage.php
@@ -46,8 +46,7 @@
 <link rel="stylesheet" href="css/owl.theme.default.min.css">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fancybox/2.1.5/jquery.fancybox.min.css" media="screen">
 </head>
-
-<div class="header_section">
+<div class="header_section_profile">
 		<nav class="navbar navbar-expand-lg navbar-light bg-light">
             <div class="logo"><a href="index.php"><img src="images/logo.png"></a></div>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -56,7 +55,7 @@
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
               <ul class="navbar-nav mr-auto">
                 <li class="nav-item">
-                  <a class="nav-link" href="index.php">HOME</a>
+                  <a class="nav-link" href="index.php">Home</a>
                 </li>
                 
                 <li class="nav-item">
@@ -85,9 +84,10 @@
                 
               </ul>
             </div>
+            
         </nav>
 	</div>
-
+	
 <body>
 	<section class="py-5 my-5">
 		<div class="container">
